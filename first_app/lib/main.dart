7f841fff8742756e8d7321209ea5cf8a8216ca07
Jaime Santos Orozco <jaime.santos.orozco@oracle.com>
2020-08-14 10:57:26 -0500

    fisrt commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Assignment 1'),
+        ),
+        body: 1 > 1
+            ? 1
+            : 1
+      ),
+    );
+  }
+}
